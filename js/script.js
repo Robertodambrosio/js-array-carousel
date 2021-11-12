7a2572fae2a099cc5fbaa92761b28596fe703d3b
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-12 14:58:21 +0100

    MILESTONE 3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
+    'img/01.jpg',  
+    'img/02.jpg', 
+    'img/03.jpg', 
+    'img/04.jpg', 
+    'img/05.jpg'  
 ];
 
 const title = [
@@ -23,9 +23,16 @@ const text = [
 ];
 
 let itemsContent = '';
+let thumbContent = '';
 
 for (let i = 0; i < items.length; i++) {
     itemsContent += `<div class="item"> 
+                    <img src="${items[i]}" alt="${title[i]}">
+                    <div class="testo">
+                    <h2>${title[i]}</h2><p>${text[i]}</p>
+                    </div>
+                </div>`;
+    thumbContent += `<div class="thumb"> 
                     <img src="${items[i]}" alt="img1">
                 </div>`;
 }
@@ -34,9 +41,44 @@ const itemsContainer = document.querySelector(".items-container");
 itemsContainer.innerHTML = itemsContent;
 
 const thumbContainer = document.querySelector(".thumbnails-container");
-thumbContainer.innerHTML = itemsContent;
+thumbContainer.innerHTML = thumbContent;
+
+let activeItem = 0;
+
+const itemsImg = document.getElementsByClassName('item');
+itemsImg[activeItem].classList.add('active');
+
+const thumbImg = document.getElementsByClassName('thumb');
+thumbImg[activeItem].classList.add('active');
 
 
-const itemsImg = document.querySelector('.item');
-itemsImg.className = 'item active';
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
 
+next.addEventListener('click', function(){
+
+    if(activeItem < items.length - 1) {
+        itemsImg[activeItem].classList.remove('active');
+        thumbImg[activeItem].classList.remove('active');
+
+        activeItem++;
+
+        itemsImg[activeItem].classList.add('active');
+        thumbImg[activeItem].classList.add('active');
+    }
+
+ })
+
+ prev.addEventListener('click', function(){
+
+ if (activeItem > 0) {
+        itemsImg[activeItem].classList.remove('active');
+        thumbImg[activeItem].classList.remove('active');
+
+        activeItem--;
+
+        itemsImg[activeItem].classList.add('active');
+        thumbImg[activeItem].classList.add('active');
+ }
+
+})
